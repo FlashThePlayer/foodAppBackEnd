fcea6b6c6458d9d4430d993f96e7e82800980c4f
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-06-09 21:08:53 +0200

    retrieve food created by the requesting user and refactor a bit of the code

--- graphql/resolvers/Food.js
+++ graphql/resolvers/Food.js
@@ -3,14 +3,14 @@ const Food = require("../../models/Food");
 const UtilError = require("../../util/Error");
 
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
-exports.createFood = async function({ foodInput }, req) {
+exports.createFood = async function ({ foodInput }, req) {
   if (!req.isAuth) {
     UtilError.throwError(401, "not authenticated!");
   }
 
-  // const user = await User.findById(req.userId);
-  const user = await User.findOne({ name: "timu" }); //only for testing
+  const user = await User.findById(req.userId);
   if (!user) {
     UtilError.throwError(401, "user not found!");
   }
@@ -22,7 +22,7 @@ exports.createFood = async function({ foodInput }, req) {
     rating: foodInput.rating,
     difficulty: foodInput.difficulty,
     creator: user,
-    keywords: foodInput.keywords
+    keywords: foodInput.keywords,
   });
 
   const dbFood = await food.save();
@@ -31,36 +31,36 @@ exports.createFood = async function({ foodInput }, req) {
     ...dbFood._doc,
     _id: dbFood._id.toString(),
     createdAt: dbFood.createdAt.toISOString(),
-    updatedAt: dbFood.updatedAt.toISOString()
+    updatedAt: dbFood.updatedAt.toISOString(),
   };
 };
 
-exports.searchFood = async function({ name }, req) {
+exports.searchFood = async function ({ name }, req) {
   if (!req.isAuth) {
     UtilError.throwError(401, "not authenticated!");
   }
 
   const dbFoods = await Food.find({
     name: name,
-    creator: new mongoose.Types.ObjectId(req.userId)
+    creator: new mongoose.Types.ObjectId(req.userId),
   });
   if (!dbFoods) {
     UtilError.throwError(404, "food not found!");
   }
 
-  const returnValue = dbFoods.map(food => {
+  const returnValue = dbFoods.map((food) => {
     return {
       ...food._doc,
       _id: food._id.toString(),
       createdAt: food.createdAt.toISOString(),
-      updatetAt: food.updatedAt.toISOString()
+      updatetAt: food.updatedAt.toISOString(),
     };
   });
 
   return returnValue;
 };
 
-exports.getRandomFood = async function(args, req) {
+exports.getRandomFood = async function (args, req) {
   if (!req.isAuth) {
     UtilError.throwError(401, "not authenticated!");
   }
@@ -75,7 +75,7 @@ exports.getRandomFood = async function(args, req) {
     ...randomFood,
     _id: randomFood._id.toString(),
     createdAt: randomFood.createdAt.toISOString(),
-    updatedAt: randomFood.updatedAt.toISOString()
+    updatedAt: randomFood.updatedAt.toISOString(),
   };
 };
 
@@ -88,21 +88,22 @@ exports.getFoods = async ({ page }, req) => {
     page = 1;
   }
   const FOODS_PER_PAGE = 5;
-  const foodCount = Food.find().countDocuments();
-  const skipFoods = page === 1 ? 0 : page * FOODS_PER_PAGE;
+  const foodCount = await Food.find().select({creator: new ObjectId(req.userId)}).countDocuments();
+  const maxPageSize = Math.ceil(foodCount / FOODS_PER_PAGE);
+  const skipFoods = (page - 1) * FOODS_PER_PAGE;
   const foods = await Food.find()
     .sort({ createdAt: -1 })
     .skip(skipFoods)
     .limit(FOODS_PER_PAGE);
   return {
-    foods: foods.map(food => {
+    foods: foods.map((food) => {
       return {
         ...food._doc,
         _id: food._id.toString(),
         createdAt: food.createdAt.toISOString(),
-        updatetAt: food.updatedAt.toISOString()
+        updatetAt: food.updatedAt.toISOString(),
       };
     }),
-    totalPages: foodCount
+    totalPages: maxPageSize,
   };
 };
