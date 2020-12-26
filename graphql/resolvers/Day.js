1ede02d6106c91355eca09f7ce993264d9cc7053
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-12-26 20:23:09 +0100

    add code for the day patch method

--- graphql/resolvers/Day.js
+++ graphql/resolvers/Day.js
@@ -6,6 +6,46 @@ const Query = require("../../util/QueryBuilder");
 
 const mongoose = require("mongoose");
 
+exports.patchDay = async function ({ dayInput }, req) {
+  if (!req.isAuth) {
+    UtilError.throwError(401, "not authenticated!");
+  }
+
+  const user = await User.findById(req.userId);
+  if (!user) {
+    UtilError.throwError(401, "user not found!");
+  }
+
+  const dbDay = await Day.findOne({
+    date: dayInput.date,
+    creator: new mongoose.Types.ObjectId(req.userId),
+  });
+
+  const foodPromises = dayInput.foodId.map(foodId => Food.findById(foodId))
+  const foodArray = await Promise.all(foodPromises);
+
+  let day;
+
+  if (!dbDay) {
+    day = new Day({
+      date: dayInput.date,
+      creator: user,
+      foods: foodArray,
+    });
+  } else {
+    day = dbDay;
+    day.foods = foodArray;
+  }
+
+  const savedDay = await day.save();
+  const populatedDay = await Day.populate(savedDay, { path: "foods" });
+
+  return {
+    date: _yyyymmdd(populatedDay.date),
+    meals: populatedDay.foods,
+  };
+}
+
 exports.deleteFoodFromDay = async function ({ dayInput }, req) {
   if (!req.isAuth) {
     UtilError.throwError(401, "not authenticated!");
