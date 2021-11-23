0a2c82467b4a1c8141ac7a1db832bccd03827c62
Timucin Acar <timucin.acar@cologne-intelligence.de>
2021-11-23 17:26:30 +0100

    update startup script

--- startup.sh
+++ startup.sh
@@ -9,11 +9,13 @@ else
   env=$1
 fi
 
-mongo_user='timu'
-mongo_pass='random'
-mongo_database='foodApp'
-node_env=${env}
-express_port=3000
-mongo_url='database:27071'
+#set env if given which is default should be dev
+export MONGO_USER=foodAppUser
+export MONGO_PASS=secret
+export NODE_ENV=${env}
+export EXPRESS_PORT=3000
+export AUTH_TOKEN_SEC="dockerStage_!_456"
+export MONGO_URI="mongodb://foodAppUser:secret@database:27017/foodApp?authSource=admin"
 
+docker-compose build
 docker-compose up
